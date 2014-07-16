fd34dbccbe8ed736b9783e51f5e82cda3c154acd
Paul Yi <paul.yi326@gmail.com>
2014-07-15 20:12:51 -0700

    have the tinder like swiping down, kind of

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,8 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('icebreaker', ['ionic']);
+
+var app = angular.module('icebreaker', ['ionic', 'openfb']);
 
 app.run(function($ionicPlatform, $rootScope) {
   $ionicPlatform.ready(function() {
@@ -122,28 +123,3 @@ app.run(function($ionicPlatform, $rootScope) {
 
 })
 
-// .controller('CardsCtrl', function($scope, $ionicSwipeCardDelegate) {
-
-//   $scope.cards = Array.prototype.slice.call(cardTypes, 0, 0);
-
-//   $scope.cardSwiped = function(index) {
-//     $scope.addCard();
-//   };
-
-//   $scope.cardDestroyed = function(index) {
-//     $scope.cards.splice(index, 1);
-//   };
-
-//   $scope.addCard = function() {
-//     var newCard = cardTypes[Math.floor(Math.random() * cardTypes.length)];
-//     newCard.id = Math.random();
-//     $scope.cards.push(angular.extend({}, newCard));
-//   }
-// })
-
-// .controller('CardCtrl', function($scope, $ionicSwipeCardDelegate) {
-//   $scope.goAway = function() {
-//     var card = $ionicSwipeCardDelegate.getSwipebleCard($scope);
-//     card.swipe();
-//   };
-// });
