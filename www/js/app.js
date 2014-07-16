0472ce910ce0f64108fb95ea2772cd18f3ca9f93
Paul Yi <paul.yi326@gmail.com>
2014-07-15 20:12:51 -0700

    have the tinder like swiping down, kind of

--- www/js/app.js
+++ www/js/app.js
@@ -98,3 +98,29 @@ app.run(function($ionicPlatform, $rootScope) {
 
 
 })
+
+.controller('CardsCtrl', function($scope, $ionicSwipeCardDelegate) {
+
+  $scope.cards = Array.prototype.slice.call(cardTypes, 0, 0);
+
+  $scope.cardSwiped = function(index) {
+    $scope.addCard();
+  };
+
+  $scope.cardDestroyed = function(index) {
+    $scope.cards.splice(index, 1);
+  };
+
+  $scope.addCard = function() {
+    var newCard = cardTypes[Math.floor(Math.random() * cardTypes.length)];
+    newCard.id = Math.random();
+    $scope.cards.push(angular.extend({}, newCard));
+  }
+})
+
+.controller('CardCtrl', function($scope, $ionicSwipeCardDelegate) {
+  $scope.goAway = function() {
+    var card = $ionicSwipeCardDelegate.getSwipebleCard($scope);
+    card.swipe();
+  };
+});
