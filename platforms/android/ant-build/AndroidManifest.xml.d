2255c37a31e531ef83a9144219fce9ccf5f222b8
Paul Yi <paul.yi326@gmail.com>
2014-07-22 20:40:21 -0700

    removed platform files

--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/Paul/Desktop/Icebreaker/icebreakerClient/platforms/android/ant-build/AndroidManifest.xml : \
-/Users/Paul/Desktop/Icebreaker/icebreakerClient/platforms/android/AndroidManifest.xml \
